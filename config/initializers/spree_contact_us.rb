95fe840c8a02a9a29fa21f549bd6aff953b9cf79
abhimediratta <abhimediratta@gmail.com>
2015-10-25 19:02:03 +0530

    change for mailer

--- config/initializers/spree_contact_us.rb
+++ config/initializers/spree_contact_us.rb
@@ -7,7 +7,7 @@ SpreeContactUs.setup do |config|
   # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
   # Example:
   # config.mailer_from = "contact@please-change-me.com"
-  config.mailer_from = "help@lahorilal.com"
+  config.mailer_from = "abhimediratta@gmail.com"
 
   # Configure the e-mail address which should receive the contact form email notifications.
   config.mailer_to = "abhimediratta@gmail.com"
